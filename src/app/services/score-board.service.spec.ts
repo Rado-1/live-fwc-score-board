13b3668f3efe3e5b56fef7f02ad2c46a4c091c41
Radovan Cervenka <radovan.cervenka@gmail.com>
2022-06-19 19:53:24 +0200

    Tests implemented

--- src/app/services/score-board.service.spec.ts
+++ src/app/services/score-board.service.spec.ts
@@ -13,13 +13,70 @@ describe('ScoreBoardService', () => {
     expect(service).toBeTruthy();
   });
 
-  it('should allow to start a game');
+  it('should allow to start a game', (done) => {
+    // successfully start game
+    const newMatch = service.startGame('Argentina', 'Belgium');
+    expect(newMatch).toBeTruthy();
 
-  it('should avoid starting incorrect game');
+    // successfully updated score board - contains the new match
+    service.scoreBoard$.subscribe((scoreBoard) => {
+      expect(scoreBoard).toContain(newMatch!);
+      done();
+    });
+  });
 
-  it('should allow to update match score');
+  it('should avoid starting incorrect game', () => {
+    // avoid entering match with the same team
+    expect(service.startGame('Argentina', 'Argentina')).toBeNull();
+  });
 
-  it('should avoid updating incorrect match score');
+  it('should allow to update match score', (done) => {
+    // successfully create match
+    let match = service.startGame('Argentina', 'Belgium');
+    expect(match).toBeTruthy();
 
-  it('should allow to finish a game');
+    // successfully update score
+    match = { ...match!, homeTeamScore: 111, awayTeamScore: 222 };
+    expect(service.updateScore(match)).toBeTrue();
+
+    // successfully updated score board - score is updated
+    service.scoreBoard$.subscribe((scoreBoard) => {
+      const updatedMatch = scoreBoard.find(
+        (m) => m.timestamp === match?.timestamp
+      );
+
+      expect(updatedMatch?.homeTeamScore).toBe(111);
+      expect(updatedMatch?.awayTeamScore).toBe(222);
+      done();
+    });
+  });
+
+  it('should avoid updating incorrect match score', () => {
+    // successfully create match
+    let match = service.startGame('Argentina', 'Belgium');
+    expect(match).toBeTruthy();
+
+    // avoid update match with negative score
+    match = { ...match!, homeTeamScore: -3 };
+    expect(service.updateScore(match)).toBeFalse();
+
+    // avoid update match with decimal score
+    match = { ...match!, homeTeamScore: 1.11 };
+    expect(service.updateScore(match)).toBeFalse();
+  });
+
+  it('should allow to finish a game', (done) => {
+    // successfully create match
+    const match = service.startGame('Argentina', 'Belgium');
+    expect(match).toBeTruthy();
+
+    // successfully finish game
+    service.finishGame(match!);
+
+    // successfully updated score board - the match is removed
+    service.scoreBoard$.subscribe((scoreBoard) => {
+      expect(scoreBoard).not.toContain(match!);
+      done();
+    });
+  });
 });
